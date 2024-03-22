96fd0828f240d285dffe5697c16b994857f4ba97
shinying0419 <lishinying05@gmail.com>
2024-03-22 11:56:26 +0800

    week05

---
+++ week05/week05-5.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        a=[]
+        while head != None:
+            a.append(head.val)
+            head=head.next
+        #print(a)
+
+        N = len(a)
+        for i in range(N):
+            if a[i] != a[N-1-i]: return False
+        return True
