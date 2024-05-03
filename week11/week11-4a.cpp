fac51ed97b6b020a8db5bde5e6cc40a67a63b1bc
shinying0419 <lishinying05@gmail.com>
2024-05-03 11:47:09 +0800

    week11

---
+++ week11/week11-4a.cpp
@@ -0,0 +1,13 @@
+//Part 1:input
+#include <stdio.h>
+int main()
+{
+	int a[10][10];
+	int m,n;
+	scanf("%d %d",&m,&n);
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+}
