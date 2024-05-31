8eac88bb7f9e21f3565b4e2b489db17198bbe6ad
shinying0419 <lishinying05@gmail.com>
2024-05-31 11:38:35 +0800

    week15

---
+++ week15/week15-3.cpp
@@ -0,0 +1,11 @@
+int equalSubstring(char* s, char* t, int maxCost) {
+    int ans=0;
+
+    int N = stalen(s);
+    for(int i=0;i<N;i++){
+        printf("%c %c\n", s[i],t[i]);
+    }
+
+    return ans;
+    
+}
---
+++ week15/week15-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+                N=len(s)
+        for i in range(N): #先用python把字串的迴圈寫出來
+            print( s[i],t[i])
+
+        ans=0
+        return ans
+        
